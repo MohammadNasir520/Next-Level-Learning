6e047d64c7b141cfe63338d551bd05a99f01fab9
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-07 12:30:49 +0600

    in guard

--- src/Module4-ObjectOrientedProgramming/type-guard.ts
+++ src/Module4-ObjectOrientedProgramming/type-guard.ts
@@ -10,5 +10,27 @@ function add(param1: Alphanumeric, param2: Alphanumeric): Alphanumeric {
   }
 }
 
-console.log(add(1, 4));
-console.log(add(1, "4"));
+// console.log(add(1, 4));
+// console.log(add(1, "4"));
+
+// in guard
+type NormalUserType = {
+  name: string;
+};
+
+type AdminUserType = {
+  name: string;
+  role: "admin";
+};
+function getUser(user: NormalUserType | AdminUserType) {
+  if ("role" in user) {
+    return ` hello My role is ${user.role}`;
+  } else {
+    return ` I am  a norma user`;
+  }
+}
+const normalUser1: NormalUserType = { name: " mr. Kallu" };
+const adminUser1: AdminUserType = { name: "mr. Gallu", role: "admin" };
+
+console.log(getUser(normalUser1));
+console.log(getUser(adminUser1));
