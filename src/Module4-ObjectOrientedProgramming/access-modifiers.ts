53a04991a2689e0ce52a8df556bc68579c547d2f
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-07 13:47:21 +0600

    access modifiers

---
+++ src/Module4-ObjectOrientedProgramming/access-modifiers.ts
@@ -0,0 +1,28 @@
+class BankAccount {
+  id: number;
+  name: string;
+  private balance: number;
+  constructor(id: number, name: string, balance: number) {
+    this.id = id;
+    this.name = name;
+    this.balance = balance;
+  }
+  getBalance() {
+    console.log(`my current balance is ${this.balance}`);
+  }
+}
+
+const myAccount = new BankAccount(38, "Nasir", 87493);
+console.log(myAccount);
+
+myAccount.getBalance();
+
+class studentAccount extends BankAccount {
+  test() {
+    this.getBalance;
+    this.name;
+    this.id;
+    // here balance is not available because balance is private
+    // and balance will not be available outside of its own class.
+  }
+}
