51f863f45238781c1e1a79c449328c527b6a83a3
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-07 12:20:44 +0600

    key of guard

---
+++ src/Module4-ObjectOrientedProgramming/type-guard.ts
@@ -0,0 +1,14 @@
+// keyof guard
+
+type Alphanumeric = string | number;
+
+function add(param1: Alphanumeric, param2: Alphanumeric): Alphanumeric {
+  if (typeof param1 === "number" && typeof param2 == "number") {
+    return param1 + param2;
+  } else {
+    return param1.toString() + param2.toString();
+  }
+}
+
+console.log(add(1, 4));
+console.log(add(1, "4"));
