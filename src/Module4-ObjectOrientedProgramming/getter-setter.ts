82bf3f8e88285ca9d58b772a763b92872d450ac3
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-08 15:45:28 +0600

    static

--- src/Module4-ObjectOrientedProgramming/getter-setter.ts
+++ src/Module4-ObjectOrientedProgramming/getter-setter.ts
@@ -1,34 +1,24 @@
-class BankAccount {
-  id: number;
-  name: string;
-  private balance: number;
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this.balance = balance;
-  }
-  // getter
+// class BankAccount {
+//   id: number;
+//   name: string;
+//   private balance: number;
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this.balance = balance;
+//   }
+//   // getter
 
-  get _balance(): number {
-    return this.balance;
-  }
-  // setter
-  set deposit(ammount: number) {
-    this.balance = this.balance + ammount;
-  }
+//   get _balance(): number {
+//     return this.balance;
+//   }
+//   // setter
+//   set deposit(ammount: number) {
+//     this.balance = this.balance + ammount;
+//   }
+// }
 
-  //.................................
-  getBalance(): number {
-    return this.balance;
-  }
-}
+// const myAccount = new BankAccount(38, "Nasir", 50);
 
-const myAccount = new BankAccount(38, "Nasir", 50);
-// console.log(myAccount);
-
-class studentAccount extends BankAccount {
-  test() {}
-}
-
-myAccount.deposit = 30;
-console.log(myAccount._balance);
+// myAccount.deposit = 30; // no need to call setter , just assign value like variable.
+// console.log(myAccount._balance);
